7fe0a93d3994fadd3e8021c51a52536ebdf7c032
Barsaiyaps <pankajbars@gmail.com>
2024-07-18 08:02:42 +0530

    added

---
+++ BACKEND/DAY5/Assigment1/index.js
@@ -0,0 +1,31 @@
+const http=require("http")
+const PORT=8080;
+const fs=require("fs")
+const path=require("path")
+
+let folder = fs.readdirSync(__dirname,"utf-8");
+console.log(folder)
+
+const server =http.createServer((req,res)=>{
+    if(req.url=="/"){
+        folder.forEach((ele)=>{
+        // res.write(`<h1>${ele}</h1>`);
+        if(fs.statSync(path.join(__dirname,ele)).isDirectory()){
+            res.write(`<a href=${ele}><li>&#128193 ${ele} </li></a>`)
+        }
+        else{
+            res.write(`<a href=${ele}><li>&#128462 ${ele} </li></a>`)
+        }
+    });
+    res.end();
+}
+else{
+    const reqPath= req.url;
+
+    res.write(reqPath);
+    res.end();
+}
+});
+
+
+server.listen(PORT)
